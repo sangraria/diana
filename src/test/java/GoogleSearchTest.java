1d4bc1bae6fa2b65c75c3184942ebfd5a0962077
sangraria <diana.pataki@gmail.com>
2023-05-04 19:58:04 +0300

    commit

--- src/test/java/GoogleSearchTest.java
+++ src/test/java/GoogleSearchTest.java
@@ -23,6 +23,22 @@ public class GoogleSearchTest {
         System.out.println("Page title is: " + driver.getTitle());
         driver.quit();
 
+        // public WebDriver driver = new ChromeDriver();
+        //
+        //    @Test
+        //    public void verifyHomePageTitle() {
+        //        driver.get(baseUrl);
+        //
+        //        WebElement cookies = driver.findElement(By.id("L2AGLb"));
+        //        cookies.click();
+        //
+        //        WebElement element = driver.findElement(By.xpath("XDyW0e"));
+        //
+        //        element.click();
+        //
+        //        System.out.println("Page title is: " + driver.getTitle());
+        //        driver.quit();
+
     }
 
 
